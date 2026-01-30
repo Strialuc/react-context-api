c19da43e6978acfcbc1022203271f7649b51e501
Luca Striano <luca.striano90@gmail.com>
2026-01-30 17:16:42 +0100

    commit

--- src/components/contexts/BudgetContext.jsx
+++ src/components/contexts/BudgetContext.jsx
@@ -1,1 +1,32 @@
+// 
+
 import { createContext, useContext, useState, useEffect } from "react";
+
+const BudgetContext = createContext();
+
+function BudgetProvider({ children }) {
+
+    //var di stato 
+
+    const [budgetMode, setBudgetMode] = useState(false)
+
+    return (
+        <BudgetContext.Provider
+            value={{
+                budgetMode,
+                setBudgetMode
+            }}>
+            {children}
+        </BudgetContext.Provider>
+
+    );
+}
+
+//hook per il contesto
+
+function useBudget() {
+    const context = useContext(BudgetContext)
+    return context;
+}
+
+export { BudgetProvider, useBudget }
